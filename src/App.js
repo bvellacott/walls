b1ec54f7aeaa445e904aabf692c68b3a97ececb9
Benjamin Vellacott <bmvellacott@gmail.com>
2017-08-27 02:18:28 +0100

    add text node

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { element, Div, text } from 'bdom'
 import { dispatch, subscribe, getState, bind } from './store'
 import { navigate } from './history/actions'
+import { keepOnParentStart, keepOnParentEnd } from 'bdom-keep-order'
 import logo from './logo.svg'
 import './App.css'
 
@@ -8,19 +9,31 @@ class App extends Div {
   constructor(props) {
     super(props);
     this.className = 'App'
-    this.currentLocation = text('?')
+    this.currentLocation = text('/King-Dom')
+
+    const list = <ul/>
+    this.keeper = keepOnParentStart(list, [
+      { condition: path => path !== '/King-Dom', 
+        elements: <li><a onclick={ () => dispatch(navigate('/King-Dom')) }>Go to King-Dom</a></li> },
+      { condition: path => path !== '/React', 
+        elements: <li><a onclick={ () => dispatch(navigate('/React')) }>Go to React</a></li> },
+      { condition: path => path !== '/Ember', 
+        elements: <li><a onclick={ () => dispatch(navigate('/Ember')) }>Go to Ember</a></li> },
+      { condition: path => path !== '/Angular', 
+        elements: <li><a onclick={ () => dispatch(navigate('/Angular')) }>Go to Angular</a></li> },
+      { condition: path => path !== '/Backbone', 
+        elements: <li><a onclick={ () => dispatch(navigate('/Backbone')) }>Go to Backbone</a></li> },
+      { condition: path => path !== '/Knockout', 
+        elements: <li><a onclick={ () => dispatch(navigate('/Knockout')) }>Go to Knockout</a></li> },
+      { condition: path => path !== '/Text-Node', 
+        elements: text("I'm a text node") },
+    ])
+
     this.appendChild(
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to Dynamo</h2>
-        <ul>
-          <li><a onclick={ () => dispatch(navigate('/BDom')) }>Go to BDom</a></li>
-          <li><a onclick={ () => dispatch(navigate('/React')) }>Go to React</a></li>
-          <li><a onclick={ () => dispatch(navigate('/Ember')) }>Go to Ember</a></li>
-          <li><a onclick={ () => dispatch(navigate('/Angular')) }>Go to Angular</a></li>
-          <li><a onclick={ () => dispatch(navigate('/Backbone')) }>Go to Backbone</a></li>
-          <li><a onclick={ () => dispatch(navigate('/Knockout')) }>Go to Knockout</a></li>
-        </ul>
+        <h2>Welcome to King-Dom</h2>
+        {list}
         <h3>{this.currentLocation}</h3>
       </div>
     )
@@ -29,10 +42,12 @@ class App extends Div {
         To get started, edit <code>src/App.js</code> and save to reload.
       </p>
     )
+    this.keeper('/King-Dom')
   }
 
   render = bind((state) => {
-    this.currentLocation.nodeValue = getState().path
+    this.currentLocation.nodeValue = state.path
+    this.keeper(state.path)
   })
 }
 
